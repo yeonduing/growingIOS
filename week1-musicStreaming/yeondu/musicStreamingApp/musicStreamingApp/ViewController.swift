5a21f4fc81b861273cabf0a8c418f15f91121535
= <yeonduing@gmail.com>
2020-08-23 14:11:52 +0900

    add week2 image

---
+++ week1-musicStreaming/yeondu/musicStreamingApp/musicStreamingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  musicStreamingApp
+//
+//  Created by 류연수 on 2020/08/16.
+//  Copyright © 2020 Yeonduing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
